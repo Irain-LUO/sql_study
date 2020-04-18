ace77e492a98c6cd690268bde920e656fd514bfa
Irain <48425577+Irain-LUO@users.noreply.github.com>
2020-04-18 22:47:15 +0800

    sql table
    using sql shell needs these sql tabel, provising an environment of  sql shell

---
+++ sql_study/sql table/dept.sql
@@ -0,0 +1,22 @@
+﻿# Host: 127.0.0.1:3366  (Version 5.5.36)
+# Date: 2020-04-18 22:44:46
+# Generator: MySQL-Front 6.1  (Build 1.26)
+
+
+#
+# Structure for table "dept"
+#
+
+DROP TABLE IF EXISTS `dept`;
+CREATE TABLE `dept` (
+  `DEPTNO` int(2) NOT NULL,
+  `DNAME` varchar(14) DEFAULT NULL,
+  `LOC` varchar(13) DEFAULT NULL,
+  PRIMARY KEY (`DEPTNO`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# Data for table "dept"
+#
+
+INSERT INTO `dept` VALUES (10,'ACCOUNTING','NEW YOURK'),(20,'RESEARCH','DALLAS'),(30,'SALES','CHICAGO'),(40,'OPERATIONS','BOSTON');
---
+++ sql_study/sql table/salgrade.sql
@@ -0,0 +1,21 @@
+﻿# Host: 127.0.0.1:3366  (Version 5.5.36)
+# Date: 2020-04-18 22:45:03
+# Generator: MySQL-Front 6.1  (Build 1.26)
+
+
+#
+# Structure for table "salgrade"
+#
+
+DROP TABLE IF EXISTS `salgrade`;
+CREATE TABLE `salgrade` (
+  `GRADE` int(11) DEFAULT NULL,
+  `LOSAL` int(11) DEFAULT NULL,
+  `HISAL` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# Data for table "salgrade"
+#
+
+INSERT INTO `salgrade` VALUES (1,700,1200),(2,1201,1400),(3,1401,2000),(4,2001,3000),(5,3001,9999);
