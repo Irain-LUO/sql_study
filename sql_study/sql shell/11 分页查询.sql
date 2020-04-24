7e3e66d927123111f8eff1a0740e7036c7eb5b61
Irain <48425577+Irain-LUO@users.noreply.github.com>
2020-04-24 11:30:23 +0800

    Add files via upload

---
+++ sql_study/sql shell/11 分页查询.sql
@@ -0,0 +1,28 @@
+﻿##进阶8 分页查询
+/*
+应用场景：一页显示不全，分页提交sql请求
+语法：
+select 查询列表 
+from 表1
+【join type】 join 表2
+on 连接条件
+where 筛选条件
+group by 分组后的筛选
+order by 排序字段
+limit offset， size;   
+##  offset：显示条目的其实索引，（起始索引从0开始）
+##  size：显示条目个数
+特点：
+1 limit语句放在查询语句的最后
+2 公式：limit （page - 1）*size，size （page：显示页数，size：每页条数）
+ 
+*/
+
+#### a 查询前五条员工信息 
+select * from emp limit 0,5;
+# 相同效果
+select * from emp limit 5;  
+#### b 查询第11条---第25条
+select * from emp limit 10,15;
+#### c 查询有奖金的员工新，并且工资较高的前2名
+select * from emp where comm is not null order by sal desc limit 0,2;
---
+++ sql_study/sql shell/12 联合查询.sql
@@ -0,0 +1,31 @@
+﻿# 进阶12 联合查询
+/*
+union联合、合并：讲多条查询语句的结果集合成一个结果集
+语法：
+查询语句1
+union
+查询语句 2
+union
+。。。。
+应用场景：查询的结果集来自多个表，且多个表没有直接的连接关系，但查询信息一致
+特点：
+1 要求多条查询语句的查询列数一致
+2 要求多条查询语句的查询的每一列的类型和顺序最好一致
+3 union关键字默认去重。使用union all包含重复项
+*/
+
+##### a查询部门编号为20 或 员工工资 > 2000 的员工信息
+select * from emp where deptno = 20 or sal > 2000;
+##### 用union实现a
+select * from emp where  sal > 2000
+union
+select * from emp where deptno = 20 ;
+##### b 查询emp表的员工工资<2000 和emp_copy表的员工工资>3000的员工名和工资
+select ename, sal from emp where sal < 2000
+union
+select ename, sal from emp_copy where sal >3000;
+
+##### b 查询emp表的员工工资<2000 和emp_copy表的员工工资<3000的员工名和工资
+select ename, sal from emp where sal < 2000
+union all #  去重
+select ename, sal from emp_copy where sal < 3000;
