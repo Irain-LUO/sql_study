29037029880a6e24f695dbfb06225abffe6a9424
Irain <48425577+Irain-LUO@users.noreply.github.com>
2020-04-28 14:14:42 +0800

    Add files via upload

---
+++ sql_study/sql shell/17 标识列.sql
@@ -0,0 +1,38 @@
+﻿# 17 标识列
+
+/*
+含义（又称自增长列）：可以不用动手的插入值，系统提供默认的序列值
+特点：
+1 标识列必须与key（主键、外键、唯一）搭配
+2 一个表中只有一个标识列
+3 标识列的类型：只能是数值型
+4 通过set auto_increment_increment语句修改标识列的步长
+5 通过set auto_increment_offset语句修改标识列的起始值，也可以手动插入。  
+# 在MySQL修改自增长列的起始值，没有起作用
+*/
+
+### 2 创建表时设置标识列
+drop table if exists tab_identity;
+create table tab_identity(
+     id int primary key auto_increment,  
+     N varchar(10)
+     );
+truncate table tab_identity;
+set auto_increment_increment = 3;  #  修改自增长列的步长
+set auto_increment_offset = 5;  ## 在MySQL修改自增长列的起始值，没有起作用
+insert into tab_identity(N) values('ss');
+insert into tab_identity(N) values('ss');
+insert into tab_identity(N) values('ss');
+show  variables like '%auto_increment%';    # 查看自增长设置
+select * from tab_identity;  
+### 3 修改表时设置标识列
+drop table if exists tab_identity;
+create table tab_identity(
+     id int ,  
+     N varchar(10)
+     );
+alter table tab_identity modify column id int primary key auto_increment;
+desc tab_identity;
+### 4 修改表时删除标识列 
+alter table tab_identity modify column id int;
+desc tab_identity;
---
+++ sql_study/sql shell/18 TCL 事务控制语言和事务并发 -.sql
@@ -0,0 +1,70 @@
+﻿#  18 TCL 事务控制语言和事务并发
+
+/*
+### 1 事务 Transaction Control Language 
+含义：一个或一组sql语句注册一个执行单元，这个执行单元要么全部执行，要么全部不执行
+特性：原子性、一致性、隔离性、持久性（ACID）
+*/
+
+### 2 事务的创建
+
+/*
+隐式事务：事务没有明显的开启和结束的标记
+比如：insert、update、delete语句
+显式事务：事务有明显的开启和结束的标记
+前提：必须先设置自动提交功能为禁用
+set autocommit = 0; # 关闭自动提交，当前会话有效
+显式事务步骤：
+步骤1：set autocommit = 0; 或 start transaction；可选
+步骤2：编写事务中的sql语句（select、inset、update、delete）
+步骤3：结束事务。commit：提交事务； rollback：回滚事务。
+savepoint  节点名； 设置保存点
+
+事务的隔离级别：read uncommitted、read committed、repeatable read、serializable
+MySQL默认：repeatable read
+Oracle默认：read committed
+查看事务隔离级别：select @@tx_isolation;
+设置隔离级别：set session/global transaction isolation level 隔离级别；
+
+*/
+drop table if exists tab;
+create table if not exists tab(
+    id int,
+    varname varchar(10)
+);
+
+#### 2.1 成功提交事务
+set autocommit = 0;
+start transaction;
+insert into tab values(1,'aa');#  编写一组事务的语句
+insert into tab values(2,'bb');
+commit; # 结束事务
+select * from tab;
+
+#### 2.2 回滚事务
+set autocommit = 0;
+start transaction;
+insert into tab values(3,'cc');#  编写一组事务的语句
+insert into tab values(4,'dd');
+rollback; # 回滚事务
+select * from tab;
+#### 2.3 savepoint与rollback搭配
+select * from tab;
+set autocommit=0;
+start transaction;  # 开启事务
+delete from tab where id=3;  # 成功删除id=3的数据
+savepoint id1;  # rollback回滚到位置，仍然执行删除id=3数据的指令
+delete from tab where id=4;  # 因为rollback回滚，没有删除id=4的数据
+rollback to id1;  # 回滚到savepoint节点位置。rollback与savepoint之间的执行无效
+select * from tab;
+
+
+
+/*
+
+
+*/
+/*
+
+
+*/
